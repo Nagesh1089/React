6009ebcc263aaa95e1ac484cf6f063dff782d613
Nagesh <nageshbsontakke@gmail.com>
2023-04-25 23:00:08 +0530

    ]

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () =>
+{
+    return(
+        <h1>This is Greate</h1>
+    )
+}
+
+
+function Footer()
+{
+    return(
+        <h1 className="FooterKaClass">Ye footer hai</h1>
+    )
+}
+
+export {Header, Footer};
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="header">
+        <h1>UseState Hook</h1>
+     </div>
+    
+
+  )
+}
+
+export default Header
